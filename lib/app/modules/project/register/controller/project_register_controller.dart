2c8442c4f5cea141abddacae43386a5a9fd18255
felipezfr <ipizfr@hotmail.com>
2022-06-21 23:05:28 -0300

    Project register Controller and State

---
+++ lib/app/modules/project/register/controller/project_register_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_job_timer_dw/app/modules/project/register/controller/project_register_state.dart';
+import 'package:bloc/bloc.dart';
+
+class ProjectRegisterController extends Cubit<ProjectRegisterState> {
+  ProjectRegisterController(super.initialState);
+}
